24641cbbf4077c3265080278028c649a52ab258f
alejandroesponda228-droid <>
2026-08-20 05:44:31 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,10 @@
+# testin
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+print("item:",item_name)
+print("price: $", item_price)
+print("quantity:", item_stock_quantity)
+print("in stock:", in_stock)
